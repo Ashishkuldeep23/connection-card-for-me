b1eac1d889a87271ce2b46f4e0531db9981aeab9
Ashishkuldeep23 <ashishkuldeep6@gmail.com>
2023-08-30 14:18:19 +0530

    Already at this page code update

--- src/components/ImPagination.jsx
+++ src/components/ImPagination.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 
-const ImPagination = ({pagesAre ,pageClickHandler ,  notificationFuction}) => {
+const ImPagination = ({ pagesAre, pageClickHandler, notificationFuction }) => {
 
 
-    const [currentPageIs , setCurrentPageIs] = useState(1)
+    const [currentPageIs, setCurrentPageIs] = useState(1)
 
     return (
         <>
@@ -13,11 +13,14 @@ const ImPagination = ({pagesAre ,pageClickHandler ,  notificationFuction}) => {
                         return <button
                             key={i}
                             style={{
-                                 margin: "5px 10px" ,
-                                 background : currentPageIs===i+1 ? "var(--dark)" : ""
-                                }}
+                                margin: "5px 10px",
+                                background: currentPageIs === i + 1 ? "var(--dark)" : ""
+                            }}
 
-                            onClick={() => { pageClickHandler(i + 1); setCurrentPageIs(i+1); notificationFuction(true, `${i + 1} page`, 0.5) }}
+                            onClick={() => {
+                                if (i + 1 === currentPageIs) return notificationFuction(true, `Already at ${i + 1}`, 0.5);
+                                pageClickHandler(i + 1); setCurrentPageIs(i + 1); notificationFuction(true, `${i + 1} page`, 0.5)
+                            }}
                         >{i + 1}</button>
                     }
                     )
